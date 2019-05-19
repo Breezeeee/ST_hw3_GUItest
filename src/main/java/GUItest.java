e97b2ba4a372cf9aaa2b97539f05b158247bcf99
Breezeeee <S-J-T-U-j-f-x@sjtu.edu.cn>
2019-05-19 19:00:13 +0800

    usage of selenium

--- src/main/java/GUItest.java
+++ src/main/java/GUItest.java
@@ -1,6 +1,8 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
@@ -13,19 +15,62 @@ public class GUItest {
         WebDriver chrome_driver = new ChromeDriver();
         chrome_driver.get("https://www.wjx.cn/jq/39655995.aspx");
 
-        WebElement text = chrome_driver.findElement(By.name("q1"));
-        text.sendKeys("test");
-
         Select select = new Select(chrome_driver.findElement(By.name("q2")));
         select.selectByIndex(1);
 
         WebElement radio = chrome_driver.findElement(By.xpath("//a[contains(@rel,'q3_3')]"));
         radio.click();
 
-        chrome_driver.findElement(By.id("divSlider4_wrapper")).click();
-        chrome_driver.switchTo().frame(60);
+        List<WebElement> checkboxes = chrome_driver.findElements(By.className("jqCheckbox"));
+        for (WebElement checkbox : checkboxes) {
+            checkbox.click();
+        }
 
-        //text.submit();
-        //chrome_driver.quit();
+        {
+            WebElement element = chrome_driver.findElement(By.id("divSlider5_bar"));
+            Actions builder = new Actions(chrome_driver);
+            builder.moveToElement(element).clickAndHold().perform();
+        }
+        {
+            WebElement element = chrome_driver.findElement(By.cssSelector(".cm:nth-child(9)"));
+            Actions builder = new Actions(chrome_driver);
+            builder.moveToElement(element).perform();
+        }
+        {
+            WebElement element = chrome_driver.findElement(By.cssSelector(".cm:nth-child(9)"));
+            Actions builder = new Actions(chrome_driver);
+            builder.moveToElement(element).release().perform();
+        }
+
+        chrome_driver.switchTo().frame(0);
+        WebElement adFileUpload = chrome_driver.findElement(By.id("fileUpload"));
+        adFileUpload.sendKeys("D:\\Proj\\SoftwareTest\\ST_hw3_GUItest\\src\\main\\resources\\1.jpg");
+        chrome_driver.switchTo().defaultContent();
+
+        List<WebElement> goods = chrome_driver.findElements(By.className("item_select"));
+        //System.out.println(goods.size());
+        WebElement good1 = goods.get(0);
+        WebElement good2 = goods.get(1);
+        WebElement good3 = goods.get(2);
+        good1.findElement(By.className("itemnum")).sendKeys("7");
+        good1.findElement(By.className("remove")).click();
+        for (int i = 0; i < 3; i++) {
+            good2.findElement(By.className("add")).click();
+        }
+        good3.findElement(By.className("itemnum")).sendKeys("-2");
+
+        chrome_driver.findElement(By.id("submit_button")).click();
+
+//        chrome_driver.quit();
+
+        Alert alert = chrome_driver.switchTo().alert();
+        alert.accept();
+
+        WebElement text = chrome_driver.findElement(By.name("q1"));
+        text.sendKeys("test");
+
+        chrome_driver.findElement(By.id("submit_button")).click();
+
+        chrome_driver.quit();
     }
 }
